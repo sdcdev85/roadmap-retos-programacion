1d4c44518744b0b374202ff769078327de0f3757
sdcdev85 <167148816+sdcdev85@users.noreply.github.com>
2024-05-09 22:05:52 -0300

    Add files via upload

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/sdcdev85.py
@@ -0,0 +1,31 @@
+# python.org
+
+# sintaxis de comentario en una linea
+
+"""sintaxis de comentario
+    en varia lineas"""
+    
+My_variable = "Sebastian"
+
+MI_CONSTANTE = "en python las constntes no existen"
+
+"""por convencion las constantes se escriben en MAYUSCULAS 
+   para indicarle al lenguaje que esa variable no debe ser modificada"""
+   
+   # Datos primitivos:
+   
+var_int = 10  # numeros enteros
+
+var_float = 1.5 #numeros fraccionarios
+
+var_str = "texto", 'texto' # Las variables str se pueden escribir entre comilla simples y comillas dobles en pytho
+
+var_bool = True, False # representran Verdadero o falso
+
+var_complex = 2.5j #variable que representa numeros complejos
+
+print("hola mundo, bienvenidos a Python!!!")
+
+
+
+
